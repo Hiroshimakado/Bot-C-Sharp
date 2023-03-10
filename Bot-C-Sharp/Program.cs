5a7e60c7d489e029b8ae45c459257913ea027f2c
Hiro <hiroshimakado@gmail.com>
2023-03-10 12:32:32 +0300

    Добавьте файлы проекта.

---
+++ Bot-C-Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace Bot_C_Sharp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            var botClient = new TelegramBotClient("6021502100:AAHOP9cqKcZvxDqAckbdLDsUUGUgXBnjUlk");
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+        }
+    }
+}
